0d5b45e8fa7173156d583627272bf35a1729ba22
farhanahmed3223 <farhanahmed3223@proton.me>
2021-10-17 14:08:00 +0500

    wip - reading from file now

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-import openai
-import os
+import openai, os, sys
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-text = open("doc.txt").read()
-question = input("Question: ")
+filepath = sys.argv[1] if len(sys.argv) > 1 else "doc.txt"
+text = open(filepath).read()
+print(f"Loaded {len(text)} chars from {filepath}")
 
-resp = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "Answer questions based on this text: " + text},
-        {"role": "user", "content": question},
-    ],
-)
-print(resp.choices[0].message.content)
+while True:
+    question = input("\nAsk: ")
+    if question.lower() in ("quit", "exit"):
+        break
+    resp = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You answer questions using this document:\n\n" + text[:3000]},
+            {"role": "user", "content": question},
+        ],
+    )
+    print(resp.choices[0].message.content)
