69b7f832c674b453fb51dec58b96a106b6a10638
farhanahmed3223 <farhanahmed3223@proton.me>
2021-10-19 22:55:00 +0500

    it actually answers lol

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
-import openai, os, sys
+import openai, os, sys, textwrap
 
-openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+if not openai.api_key:
+    print("Set OPENAI_API_KEY first")
+    sys.exit(1)
 
-filepath = sys.argv[1] if len(sys.argv) > 1 else "doc.txt"
-text = open(filepath).read()
-print(f"Loaded {len(text)} chars from {filepath}")
-
-while True:
-    question = input("\nAsk: ")
-    if question.lower() in ("quit", "exit"):
-        break
+def ask(text: str, question: str) -> str:
     resp = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You answer questions using this document:\n\n" + text[:3000]},
+            {"role": "system", "content": f"Answer questions using this document:\n\n{text[:4000]}"},
             {"role": "user", "content": question},
         ],
     )
-    print(resp.choices[0].message.content)
+    return resp.choices[0].message.content
+
+if __name__ == "__main__":
+    path = sys.argv[1] if len(sys.argv) > 1 else "doc.txt"
+    text = open(path).read()
+    print(f"Loaded: {path} ({len(text)} chars)")
+    while True:
+        q = input("\n> ")
+        if q.lower() in ("q", "quit", "exit"):
+            break
+        print(textwrap.fill(ask(text, q), 80))
