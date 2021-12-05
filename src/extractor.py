1ee56d3cbd04dc5596056e445bcf6e47618cfdff
farhanahmed3223 <farhanahmed3223@proton.me>
2021-12-05 16:44:00 +0500

    pdf parsing mostly works

--- src/extractor.py
+++ src/extractor.py
@@ -1,19 +1,24 @@
-"""Extract text from .txt and .pdf files."""
+"""Extract plain text from .txt and .pdf files."""
 from pathlib import Path
 
-def extract_text(filepath: str) -> str:
+SUPPORTED = {".txt", ".pdf", ".md"}
+
+def extract_text(filepath: str) -> tuple[str, str]:
+    """Return (text, file_type)."""
     path = Path(filepath)
+    if path.suffix.lower() not in SUPPORTED:
+        raise ValueError(f"Unsupported file type: {path.suffix}")
     if path.suffix.lower() == ".pdf":
-        return _extract_pdf(path)
-    return path.read_text(encoding="utf-8", errors="ignore")
+        return _extract_pdf(path), "pdf"
+    return path.read_text(encoding="utf-8", errors="ignore"), "text"
 
 def _extract_pdf(path: Path) -> str:
-    try:
-        import fitz  # pymupdf
-    except ImportError:
-        raise ImportError("Install pymupdf: pip install pymupdf")
+    import fitz
     doc = fitz.open(str(path))
-    pages = []
+    out = []
     for i, page in enumerate(doc):
-        pages.append(f"[PAGE {i+1}]\n{page.get_text()}")
-    return "\n\n".join(pages)
+        text = page.get_text("text")
+        if text.strip():
+            out.append(f"[PAGE {i+1}]\n{text.strip()}")
+    doc.close()
+    return "\n\n".join(out)
