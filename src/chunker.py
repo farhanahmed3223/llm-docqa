aa2d7a81b4764032a15b22b594cfadce376cd362
farhanahmed3223 <farhanahmed3223@proton.me>
2022-03-12 14:22:00 +0500

    rewrite chunker with overlap

--- src/chunker.py
+++ src/chunker.py
@@ -1,8 +1,10 @@
-"""Split documents into overlapping chunks."""
-from dataclasses import dataclass
+"""Token-aware chunker with configurable overlap."""
+import re
+from dataclasses import dataclass, field
 
-CHUNK_CHARS = 1500
-OVERLAP_CHARS = 200
+CHUNK_SIZE = 500
+OVERLAP = 50
+ENCODING_MODEL = "cl100k_base"
 
 @dataclass
 class Chunk:
@@ -10,20 +12,58 @@ class Chunk:
     start_char: int
     end_char: int
     page_num: int | None = None
+    token_count: int = 0
 
 def chunk_text(text: str, file_type: str = "text") -> list[Chunk]:
-    if not text or not text.strip():
+    try:
+        import tiktoken
+        enc = tiktoken.get_encoding(ENCODING_MODEL)
+        return _token_chunk(text, enc, file_type)
+    except ImportError:
+        return _char_chunk(text, file_type)
+
+def _token_chunk(text: str, enc, file_type: str) -> list[Chunk]:
+    tokens = enc.encode(text)
+    if not tokens:
         return []
-    # hack: if doc is shorter than one chunk, return as-is
-    if len(text) <= CHUNK_CHARS:
-        return [Chunk(content=text, start_char=0, end_char=len(text))]
+    step = max(1, CHUNK_SIZE - OVERLAP)
     chunks = []
-    step = max(1, CHUNK_CHARS - OVERLAP_CHARS)
-    i = 0
-    while i < len(text):
-        end = min(i + CHUNK_CHARS, len(text))
-        chunks.append(Chunk(content=text[i:end], start_char=i, end_char=end))
-        if end == len(text):
+    for start in range(0, len(tokens), step):
+        end = min(start + CHUNK_SIZE, len(tokens))
+        window = tokens[start:end]
+        content = enc.decode(window)
+        start_char = len(enc.decode(tokens[:start]))
+        end_char = start_char + len(content)
+        page = _detect_page(content) if file_type == "pdf" else None
+        chunks.append(Chunk(content=content, start_char=start_char,
+                            end_char=end_char, page_num=page, token_count=len(window)))
+        if end == len(tokens):
             break
-        i += step
     return chunks
+
+def _char_chunk(text: str, file_type: str) -> list[Chunk]:
+    """Fallback if tiktoken not installed."""
+    SIZE, OVL = 1500, 150
+    if len(text) <= SIZE:
+        return [Chunk(content=text, start_char=0, end_char=len(text))]
+    chunks, i = [], 0
+    while i < len(text):
+        end = min(i + SIZE, len(text))
+        content = text[i:end]
+        chunks.append(Chunk(content=content, start_char=i, end_char=end))
+        if end == len(text): break
+        i += SIZE - OVL
+    return chunks
+
+_PAGE_RE = re.compile(r"\[PAGE (\d+)\]")
+
+def _detect_page(content: str) -> int | None:
+    m = _PAGE_RE.search(content)
+    return int(m.group(1)) if m else None
+
+def count_tokens(text: str) -> int:
+    try:
+        import tiktoken
+        return len(tiktoken.get_encoding(ENCODING_MODEL).encode(text))
+    except ImportError:
+        return len(text) // 4
