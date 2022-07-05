9c42e4b7a8b38da3d29c48a50dfc345a5327d212
farhanahmed3223 <farhanahmed3223@proton.me>
2022-07-05 21:33:00 +0500

    fix null pointer when pdf has no text layer

--- src/extractor.py
+++ src/extractor.py
@@ -1,11 +1,12 @@
-"""Extract plain text from .txt and .pdf files."""
+"""Extract plain text from .txt, .pdf, and .md files."""
 from pathlib import Path
 
 SUPPORTED = {".txt", ".pdf", ".md"}
 
 def extract_text(filepath: str) -> tuple[str, str]:
-    """Return (text, file_type)."""
     path = Path(filepath)
+    if not path.exists():
+        raise FileNotFoundError(f"File not found: {filepath}")
     if path.suffix.lower() not in SUPPORTED:
         raise ValueError(f"Unsupported file type: {path.suffix}")
     if path.suffix.lower() == ".pdf":
@@ -17,8 +18,14 @@ def _extract_pdf(path: Path) -> str:
     doc = fitz.open(str(path))
     out = []
     for i, page in enumerate(doc):
-        text = page.get_text("text")
-        if text.strip():
+        try:
+            text = page.get_text("text")
+        except Exception:
+            text = ""
+        # guard: skip pages with no text layer (e.g. scanned images)
+        if text and text.strip():
             out.append(f"[PAGE {i+1}]\n{text.strip()}")
     doc.close()
+    if not out:
+        raise ValueError("PDF has no extractable text layer (scanned image?). Try OCR first.")
     return "\n\n".join(out)
