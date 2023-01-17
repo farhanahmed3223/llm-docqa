762b0c0f579878aafbb5444492ae306f1abf938b
farhanahmed3223 <farhanahmed3223@proton.me>
2023-01-17 11:22:00 +0500

    upgrade openai sdk and default to gpt-4o-mini

--- src/chat.py
+++ src/chat.py
@@ -1,26 +1,26 @@
-"""OpenAI chat wrapper with token budget management."""
+"""OpenAI chat wrapper — upgraded to openai>=1.0 SDK."""
 import os
 from src.chunker import count_tokens
 
-DEFAULT_MODEL = "gpt-3.5-turbo"
 CONTEXT_LIMIT = {
-    "gpt-3.5-turbo": 4096,
-    "gpt-4": 8192,
-    "gpt-4-turbo-preview": 128000,
+    "gpt-3.5-turbo": 16385,
+    "gpt-4o-mini": 128000,
+    "gpt-4o": 128000,
+    "gpt-4-turbo": 128000,
 }
-SYSTEM_OVERHEAD = 300
+SYSTEM_OVERHEAD = 400
 
 def answer(context_chunks, question: str, history: list[dict], model: str) -> str:
-    import openai
-    openai.api_key = os.environ["OPENAI_API_KEY"]
-    budget = CONTEXT_LIMIT.get(model, 4096) - SYSTEM_OVERHEAD - count_tokens(question)
+    from openai import OpenAI
+    client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+    budget = CONTEXT_LIMIT.get(model, 16385) - SYSTEM_OVERHEAD - count_tokens(question)
     context = _build_context(context_chunks, budget)
     messages = [
-        {"role": "system", "content": f"Answer questions using this document:\n\n{context}"},
-        *history[-4:],  # keep last 2 exchanges
+        {"role": "system", "content": f"Answer questions using only this document:\n\n{context}"},
+        *history[-6:],
         {"role": "user", "content": question},
     ]
-    resp = openai.ChatCompletion.create(model=model, messages=messages)
+    resp = client.chat.completions.create(model=model, messages=messages)
     return resp.choices[0].message.content
 
 def _build_context(chunks, budget: int) -> str:
